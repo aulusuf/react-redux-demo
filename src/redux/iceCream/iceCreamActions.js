ff9ab8e2085e9d70ff200b5e72a741dadd98101a
Muhammad Yusuf Auliya <aulusuf@gmail.com>
2022-02-10 14:43:16 +0700

    commit

---
+++ src/redux/iceCream/iceCreamActions.js
@@ -0,0 +1,9 @@
+// second to do in redux folder
+
+import { BUY_ICECREAM } from "./iceCreamType";
+
+export const buyIceCream = () => {
+  return {
+    type: BUY_ICECREAM,
+  };
+};
---
+++ src/redux/iceCream/iceCreamType.js
@@ -0,0 +1,3 @@
+// do and fill type first in coding
+
+export const BUY_ICECREAM = "BUY_ICECREAM";
