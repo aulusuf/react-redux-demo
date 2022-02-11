c0a3567d85c257d40111900a75480509087622fb
Muhammad Yusuf Auliya <aulusuf@gmail.com>
2022-02-11 14:34:59 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./App.css";
-import HooksCakeContainer from "./components/HooksCakeContainer";
-import CakeContainer from "./components/CakeContainer";
-import NewCakeContainer from "./components/NewCakeContainer";
-import IceCreamContainer from "./components/IceCreamContainer";
-import ItemContainer from "./components/ItemContainer";
+import UserContainer from "./components/UserContainer";
+// import HooksCakeContainer from "./components/HooksCakeContainer";
+// import CakeContainer from "./components/CakeContainer";
+// import NewCakeContainer from "./components/NewCakeContainer";
+// import IceCreamContainer from "./components/IceCreamContainer";
+// import ItemContainer from "./components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <ItemContainer cake />
+        <UserContainer />
+        {/* <ItemContainer cake />
         <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
-        <NewCakeContainer />
+        <NewCakeContainer /> */}
       </div>
     </Provider>
   );
